5d4dc5130945f85a78106d4215f9599601e536f5
Anton Puchkov <puchkova@amazon.com>
2021-01-13 10:39:20 -0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Anton"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
